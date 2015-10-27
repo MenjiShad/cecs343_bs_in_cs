adfa631a61515d90389a8f8bc354eda501988596
ztbcrazy <ztbcrazy@yahoo.com>
2015-10-27 11:51:46 -0700

    Redesigned class to have 1 stack of cards
    Started hardcoding in game cards

--- Deck.java
+++ Deck.java
@@ -5,27 +5,23 @@ import javax.swing.ImageIcon;
 
 public class Deck {
 	
-	private Stack<GameCard> listOfActiveCards;
-	private Stack<GameCard> listOfDiscardedCards;
-	private List<GameCard> listOfDisabledCards;
-	private static String gameCardImage1 = "src/GameCard 1.png";
-	private static String gameCardImage2 = "src/GameCard 2.png";
-	private static String gameCardImage3 = "src/GameCard 3.png";
-	private static String gameCardImage4 = "src/GameCard 4.png";
+	private Stack<GameCard> stackOfCards;
+	//private Stack<GameCard> listOfDiscardedCards;
+	//private List<GameCard> listOfDisabledCards;
 	
 	public Deck() {
 		
 		//Creates game card images
-		GameCard gameCard1 = new GameCard("Meet The Dean", new ImageIcon(gameCardImage1));
-		GameCard gameCard2 = new GameCard("CECS 100", new ImageIcon(gameCardImage2));
-		GameCard gameCard3 = new GameCard("The Outpost", new ImageIcon(gameCardImage3));
-		GameCard gameCard4 = new GameCard("Goodbye, professor", new ImageIcon(gameCardImage4));
+//		GameCard gameCard1 = new GameCard("Meet The Dean", "src/picture/1.png");
+//		GameCard gameCard2 = new GameCard("CECS 100", "src/picture/2.png");
+//		GameCard gameCard3 = new GameCard("The Outpost", new ImageIcon(gameCardImage3));
+//		GameCard gameCard4 = new GameCard("Goodbye, professor", new ImageIcon(gameCardImage4));
 		
 		//Adds game card images to list of active cards
-		listOfActiveCards.add(gameCard1);
-		listOfActiveCards.add(gameCard2);
-		listOfActiveCards.add(gameCard3);
-		listOfActiveCards.add(gameCard4);
+//		stackOfCards.add(gameCard1);
+//		stackOfCards.add(gameCard2);
+//		stackOfCards.add(gameCard3);
+//		stackOfCards.add(gameCard4);
 	}
 	
 	public void shuffle() {
