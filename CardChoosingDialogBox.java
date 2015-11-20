ccd666c24a87ef1b1417e20fb94d9fde92701288
ztbcrazy <ztbcrazy@yahoo.com>
2015-11-20 15:45:59 -0800

    Implemented Card Chooser Using Buttons
    Either use buttons, or the JOptionPane drop down list,
    or figure out a way to use MouseListener
    NOTE: my button way doesnt update card discarded on screen

--- CardChoosingDialogBox.java
+++ CardChoosingDialogBox.java
@@ -8,13 +8,16 @@ import javax.swing.*;
 public class CardChoosingDialogBox extends JDialog{
 	
 	private JFrame frame = new JFrame("Dialog Box");
-	private JPanel dialogPanel = new JPanel();
+	private JPanel masterPanel = new JPanel();
+	//private JPanel dialogPanel = new JPanel();
+	//private JPanel buttonPanel = new JPanel();
 	private ArrayList<JLabel> images = new ArrayList<>();
+	private ArrayList<JPanel> buttonPanels = new ArrayList<>();
+	private ArrayList<JButton> buttons = new ArrayList<>();
     private static final int DIALOG_BOX_WIDTH = 800;
     private static final int DIALOG_BOX_HEIGHT = 600;
     private Player currentPlayer;
     //private static JOptionPane jp = new JOptionPane();
-    private ArrayList<GameCard> hand;
 	
 	public CardChoosingDialogBox(Player human) {
 		
@@ -22,55 +25,98 @@ public class CardChoosingDialogBox extends JDialog{
 		frame.setAlwaysOnTop(true);
 		frame.setLocationRelativeTo(null);
 		this.setModalityType(DEFAULT_MODALITY_TYPE); // Set to Modal
-		BoxLayout dialogLayout = new BoxLayout(dialogPanel, BoxLayout.X_AXIS);
-        dialogPanel.setLayout(dialogLayout); // Thinking BoxLayout
         this.setSize(DIALOG_BOX_WIDTH, DIALOG_BOX_HEIGHT); // May change
-        dialogPanel.setSize(DIALOG_BOX_WIDTH, DIALOG_BOX_HEIGHT); // May change
+        //dialogPanel.setSize(DIALOG_BOX_WIDTH, DIALOG_BOX_HEIGHT); // May change
         currentPlayer = human;
         
+        //Adding cards and button images to dialogPanel
         for(int i = 0; i < human.getHandOfCards().size(); i++ ) {
         	images.add(new JLabel(human.getHandOfCards().get(i).getCardImage()));
-        	dialogPanel.add(images.get(i));
+        	buttons.add(new JButton(human.getHandOfCards().get(i).getCardName()));
+        	buttonPanels.add(new JPanel());
+        	BoxLayout dialogLayout = new BoxLayout(buttonPanels.get(i), BoxLayout.Y_AXIS);
+            buttonPanels.get(i).setLayout(dialogLayout); // Thinking BoxLayout
+        	//buttonPanels.get(i).add(Box.createHorizontalStrut(100));
+        	buttonPanels.get(i).add(images.get(i));
+        	buttonPanels.get(i).add(buttons.get(i));
+        	masterPanel.add(buttonPanels.get(i));
+        	//dialogPanel.add(images.get(i));
+        	//buttonPanel.add(buttonPanels.get(i));
+        	
         }
+ 
         
-        final class ButtonListener implements MouseListener {
+        //masterPanel.add(dialogPanel);
+      //  masterPanel.add(buttonPanel);
+        
+//        final class ButtonMListener implements MouseListener {
+//        	GameCard selectedCard;
+//
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				Object selection = e.getComponent();
+//				System.out.println("\n\nChoose gamecard " + selection + " to discard\n\n");
+//				for(int i = 0; i < currentPlayer.getHandOfCards().size(); i++) {
+//		    		
+//	    			if(selection.equals(currentPlayer.getHandOfCards().get(i).getCardName())) {
+//	    				currentPlayer.getHandOfCards().remove(currentPlayer.getHandOfCards().get(i));
+//	    				System.out.println("\n\nTEST: removed gamecard\n\n");
+//		    		}
+//		    	}
+//				frame.setVisible(false);
+//				
+//			}
+//
+//			@Override
+//			public void mousePressed(MouseEvent e) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//
+//			@Override
+//			public void mouseReleased(MouseEvent e) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//
+//			@Override
+//			public void mouseEntered(MouseEvent e) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//
+//			@Override
+//			public void mouseExited(MouseEvent e) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//	
+//        }
+        
+        final class ButtonListener implements ActionListener {
 
 			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-				
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("\n\n" + e.getActionCommand());
+				for(int i = 0; i < currentPlayer.getHandOfCards().size(); i++) {
+		    		
+	    			if(e.getActionCommand().equals(currentPlayer.getHandOfCards().get(i).getCardName())) {
+	    				System.out.println("\n\nTESTING\n\n");
+	    				currentPlayer.getHandOfCards().remove(currentPlayer.getHandOfCards().get(i));
+		    		}
+		    	}
+				frame.setVisible(false);
 				
 			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-	
-        }
-        for(int i = 0; i < images.size();i++) {
-			images.get(i).addMouseListener(new ButtonListener());
+    		
+    	}
+        
+        //Adding a listener to each GameCard Image
+        for(int i = 0; i < buttons.size();i++) {
+			buttons.get(i).addActionListener(new ButtonListener());
 		}
         
-        frame.add(dialogPanel);
+        frame.add(masterPanel);
         frame.setVisible(true);
 		
 	}
